adbb469e47045c344bf784fa8b394aabdc246b01
Jordan Faulkner <jfaulkner2j@icloud.com>
2023-01-02 15:56:25 +0000

    Add project files.

---
+++ jCalc2/MainWindow.cpp
@@ -0,0 +1,12 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	jCalc2::MainWindow form;
+	Application::Run(% form);
+}
